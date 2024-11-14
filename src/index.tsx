4789045f902628cb7dddf9879b6e72762f33d439
Kyle Muldoon <kylemuldoon15@gmail.com>
2024-11-13 16:33:46 -0800

    [chore] update tailwind config + reset blank index.tsx to boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, {ReactElement} from "react"
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom"
 import ReactDOM from "react-dom/client"
 
 import "./index.css"
 
+function App(): ReactElement {
+  return <div className="h-screen w-screen flex items-center justify-center">Hello World!</div>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <p>Hello World!</p>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
